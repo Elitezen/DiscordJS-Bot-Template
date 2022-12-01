11c912369f40484cbf61dcc9e80a961b56443151
Elitezen <elitezen4531@gmail.com>
2022-11-30 20:04:57 -0500

    Integrated configure CLI, Basic README steps

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
+# Create Discord Bot
+
+## Setup
+```ssh
+npm i
+```
+
+```ssh
+npm run configure
+```
+
+Currently only guild scope
+```ssh
+npm run commands
+```
+
+```ssh
+npm start
+```
+
 # To Do
 - Add command line interface
 - Add handlers for remaining interaction types
 - Develop full README
-- Add Prettier
---
+++ Scripts/configure.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import { writeFile } from "node:fs";
+
+let token, clientId, guildId;
+
+async function start() {
+  await getToken();
+  await getClientId();
+  await getGuildId();
+  await createEnv();
+}
+
+async function getToken() {
+  const answers = await inquirer.prompt({
+    name: 'token',
+    type: 'input',
+    message: "Enter your client's token"
+  });
+
+  token = answers.token;
+}
+
+async function getClientId() {
+  const answers = await inquirer.prompt({
+    name: 'clientId',
+    type: 'input',
+    message: "Enter your client's id"
+  });
+
+  clientId = answers.clientId;
+}
+
+async function getGuildId() {
+  const answers = await inquirer.prompt({
+    name: 'guildId',
+    type: 'input',
+    message: "Enter your guild id"
+  });
+
+  guildId = answers.guildId;
+}
+
+async function createEnv() {
+  writeFile('.env', `CLIENT_TOKEN=${token}\nCLIENT_ID=${clientId}\nCLIENT_GUILD_ID=${guildId}`, err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    
+    console.log('Success');
+    process.exit(0);
+  });
+}
+
+start();
