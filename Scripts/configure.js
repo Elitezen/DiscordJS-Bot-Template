2bc9edba7bbb999182f9f887c936aea6c9f65abb
Elitezen <elitezen4531@gmail.com>
2022-11-30 20:13:10 -0500

    Added configure scripts

--- Scripts/configure.js
+++ Scripts/configure.js
@@ -49,7 +49,12 @@ async function createEnv() {
       process.exit(1);
     }
     
-    console.log('Success');
+    console.log(`
+      Success
+
+      npm run commands
+      npm start
+    `);
     process.exit(0);
   });
 }
